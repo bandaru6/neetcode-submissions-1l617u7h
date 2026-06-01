d1a89f4a86b57f48193051dee8a055b6947beca5
Aashrith Bandaru <bandaru6@illinois.edu>
2026-06-01 10:19:10 -0500

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_map = defaultdict(int)
+        t_map = defaultdict(int)
+
+        for char in s:
+            s_map[char] += 1
+        for char in t:
+            t_map[char] += 1
+
+        return s_map == t_map
