37f606a9c4a6f02e236daaa5f209e43747a9bac0
Aashrith Bandaru <bandaru6@illinois.edu>
2026-08-13 09:58:20 -0700

    Add: zeroOneKnapsack - submission-0

---
+++ Data Structures & Algorithms/zeroOneKnapsack/submission-0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maximumProfit(self, profit: List[int], weight: List[int], capacity: int) -> int:
+
+        memo = {}
+
+        def dfs(i, capacity):
+
+            if (i, capacity) in memo:
+                return memo[(i, capacity)]
+            if i == len(weight):
+                return 0
+            
+            skip = dfs(i+1, capacity)
+
+            take = 0
+            if weight[i] <= capacity:
+                take += profit[i] + dfs(i+1, capacity - weight[i])
+            
+            memo[(i, capacity)] = max(skip, take)
+
+            return max(skip, take)
+            
+        return dfs(0, capacity)
+
+
+
+
+
+
