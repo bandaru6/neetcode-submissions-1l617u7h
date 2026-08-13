c6ed000a74f3c442db83a2095695cddf11dac91b
Aashrith Bandaru <bandaru6@illinois.edu>
2026-08-13 10:23:24 -0700

    Add: unboundedKnapsack - submission-1

---
+++ Data Structures & Algorithms/unboundedKnapsack/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maximumProfit(self, profit: List[int], weight: List[int], capacity: int) -> int:
+        memo = {}
+
+        def dfs(i, cap):
+
+            if (i, cap) in memo:
+                return memo[(i, cap)]
+            if i >= len(weight):
+                return 0
+            
+            skip = dfs(i + 1, cap)
+
+            take = 0
+            if weight[i] <= cap:
+                take = max(dfs(i + 1, cap - weight[i]) + profit[i], dfs(i, cap - weight[i]) + profit[i])
+            
+            memo[(i, cap)] = max(skip, take)
+
+            return memo[(i, cap)]
+        
+
+        return dfs(0, capacity)
